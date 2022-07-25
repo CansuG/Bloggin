aa3d05c2b122c6e33a31b77ce37c708678326a13
CansuG <cansugurel@eskisehir.edu.tr>
2022-07-25 08:56:52 +0300

    Blog card component is created.

--- BlogLab-UI/src/app/components/blog-components/blog-card/blog-card.component.ts
+++ BlogLab-UI/src/app/components/blog-components/blog-card/blog-card.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Blog } from 'src/app/models/blog/blog.model';
+import { PhotoService } from 'src/app/services/photo.service';
 
 @Component({
   selector: 'app-blog-card',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogCardComponent implements OnInit {
 
-  constructor() { }
+  @Input() blog!: Blog;
+
+  blogPhotoUrl!: string;
+
+  constructor(
+    private router: Router,
+    private photoService: PhotoService
+  ) { }
 
   ngOnInit(): void {
+    if (!!this.blog.photoId) {
+      this.photoService.get(this.blog.photoId).subscribe(photo => {
+        if (!!photo) {
+          this.blogPhotoUrl = photo.imageUrl;
+        }
+      })
+    }
   }
 
+  readMore(blogId: number) {
+    this.router.navigate([`/blogs/${blogId}`])
+  }
 }
+
